bf4f07a083480df4620e5d970a030303370a33ec
Brian Ritter <brian@ambrdetroit.com>
2017-06-04 21:36:34 -0400

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 [ v1.0.0 ] ambrjs   
 =============
+
+Description
+-----------
+
+ambrjs is a JavaScript library to simplify the creation of simple, flexable, one-page dynamic websites through the use of:
+
+- resource driven url design pattern for commands
+- view based rendering using html templates
+- form validation and error rendering
---
+++ src/ambrjs.php
@@ -0,0 +1,28 @@
+<?php
+	// Set the proper response header
+	header('Content-type: text/javascript'); 
+?>
+
+(function() {
+	"use strict";
+	
+	window.ambrjs = window.ambrjs || {};
+	
+	<?php
+		
+		// view
+		//include("view.js");
+		
+		// command
+		//include("command.js");
+		
+		// template
+		//include("template.js");
+		
+		// form
+		//include("form.js");
+		//include("form-defaultCodesAndValidators.js");
+
+	?>
+	
+})();
